46db3be1a1425455b8b39c60646d7619e1926745
THANH0201 <160411131+THANH0201@users.noreply.github.com>
2025-08-31 00:31:51 +0300

    cm

--- SimulationIntro/src/Task5/ServicePoint.java
+++ SimulationIntro/src/Task5/ServicePoint.java
@@ -16,26 +16,24 @@ public class ServicePoint {
     public void serve(){
         while (!queueP1.isEmpty()) {
             Customer movCustomer = removeFromQueueue();
+            movCustomer.setTimeStartServed(System.nanoTime());
             System.out.printf("Customer %d is on serving at\n", movCustomer.getId());
-            double waitTime = movCustomer.timeSpend() / 1000000000;
-            long timeEnd = System.nanoTime();
-            movCustomer.setTimeEnd(timeEnd);
-            LocalDateTime displayEnd = LocalDateTime.now();
-
+            double waitTime = (movCustomer.getTimeStartServed()-movCustomer.getTimeStart())/1000000000.0;
             int serviceTime = (int) (Math.random() * 5 + 1);//second
-            double responseTime = waitTime + serviceTime;
-            System.out.printf("Customer %d done at %s:\nWait Time: %.5f s\nService Time: %d s\nResponse Time: %.5f s\n\n", movCustomer.getId(), displayEnd, waitTime, serviceTime, responseTime);
             try {
-                Thread.sleep(serviceTime);
+                Thread.sleep(serviceTime* 1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            long timeStart = System.nanoTime()/1000000000;
-            customer.setTimeStart(timeStart);
-        }
+            movCustomer.setTimeEnd(System.nanoTime());
+            LocalDateTime displayEnd = LocalDateTime.now();
+            double responseTime = waitTime + serviceTime;
+            System.out.printf("Customer %d done at %s:\nWait Time: %.5f s\nService Time: %d s\nResponse Time: %.5f s\n\n", movCustomer.getId(), displayEnd, waitTime, serviceTime, responseTime);
+            }
         System.out.println("All customer served.");
-
+        }
     }
 
-}
+
+
 
